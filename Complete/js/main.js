752ca536787117dcc7b6d3211747cc071a539413
kapansa <samuelkapansa@gmail.com>
2021-09-14 09:30:49 +0200

    Finish building structure

--- Complete/js/main.js
+++ Complete/js/main.js
@@ -1,25 +1,22 @@
-var btn_reply = document.querySelectorAll(".btn_reply");
+var replies = document.querySelectorAll(".hide_show");
 
-console.log(btn_reply);
-
-btn_reply.forEach((reply) => {
-
-    reply.addEventListener("click", function(e) {
-
-        var replies = document.querySelectorAll(".hide_show");
-        replies.forEach((hide_show)=>{
-            hide_show.classList.toggle("hide_show_replies")
-        });
-
-    });
-
+replies.forEach((reply) => {
+    reply.classList.add("hide_show_replies");
 });
 
+function HideShowReplies(id) {
+
+    var hide_show = document.getElementById(id);
+    hide_show.classList.toggle("hide_show_replies");
+
+}
 
 var likes = document.querySelectorAll(".btn_like");
 
-likes.forEach((like)=>{
-    like.addEventListener("click", function(e) {
+likes.forEach((like) => {
+
+    like.addEventListener("click", function (e) {
         this.innerHTML = "✓ Liked";
     });
+
 })
